52c694953f5c3ddb19fc7b6d7eb999b221707ef9
ayanszone <sheikhayaan470@gmail.com>
2024-03-19 18:06:18 +0000

    Updated Repository

---
+++ index.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division","Modulus","Exponentiation",],
+  },
+]);
+
+//Conditional Statement
+if(answer.operator === "Addition"){
+    console.log("Your value is: ", answer.firstNumber + answer.secondNumber);
+}
+else if(answer.operator === "Subtraction"){
+    console.log("Your value is", answer.firstNumber - answer.secondNumber);
+}
+else if(answer.operator === "Multiplication"){
+    console.log("Your value is", answer.firstNumber * answer.secondNumber);
+}
+else if(answer.operator === "Division"){
+    console.log("Your value is", answer.firstNumber / answer.secondNumber);
+}
+else if(answer.operator === "Modulus"){
+    console.log("Your value is", answer.firstNumber % answer.secondNumber);
+}
+else if(answer.operator === "Exponentiation"){
+    console.log("Your value is", answer.firstNumber ** answer.secondNumber);
+}
+else{
+    console.log("Please select valid operator");
+}
